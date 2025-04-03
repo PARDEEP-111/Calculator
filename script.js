076a0bbe6c1e232a149d2f615967c9b5c0b28da6
PARDEEP 111 <pardeepsingh091209@gmail.com>
2025-04-03 18:34:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
- let button = document.querySelector(".one")
-        button.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="1"
-        })
+//  let button = document.querySelector(".one")
+//         button.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="1"
+//         })
     
-        let button2 = document.querySelector(".two")
-        button2.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="2"
-        })
+//         let button2 = document.querySelector(".two")
+//         button2.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="2"
+//         })
         
-        let button3 = document.querySelector(".three")
-        button3.addEventListener("click", e=>{
+//         let button3 = document.querySelector(".three")
+//         button3.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="3"
+//         })
+//         let button4 = document.querySelector(".four")
+//         button4.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="4"
+//         })
+//         let button5 = document.querySelector(".five")
+//         button5.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".five").innerHTML="5"
+//         })
+//         let button6 = document.querySelector(".six")
+//         button6.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="6"
+//         })
+//         let button7 = document.querySelector(".seven")
+//         button7.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="7"
+//         })
+//           let button8 = document.querySelector(".eight")
+//         button8.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="8"
+//         })
+//           let button9 = document.querySelector(".nine")
+//         button9.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="9"
+//         }) 
+//          let button0 = document.querySelector(".zero")
+//         button0.addEventListener("click", e=>{
+//             e.stopPropagation()
+//          document.querySelector(".screen").innerHTML="0"
+//         })
+function main(integer,clas,number,){
+
+let item = ` let ${integer} = document.querySelector(".${clas}")
+        ${integer}.addEventListener("click", e=>{
             e.stopPropagation()
-         document.querySelector(".screen").innerHTML="3"
-        })
-        let button4 = document.querySelector(".four")
-        button4.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="4"
-        })
-        let button5 = document.querySelector(".five")
-        button5.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".five").innerHTML="5"
-        })
-        let button6 = document.querySelector(".six")
-        button6.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="6"
-        })
-        let button7 = document.querySelector(".seven")
-        button7.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="7"
-        })
-          let button8 = document.querySelector(".eight")
-        button8.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="8"
-        })
-          let button9 = document.querySelector(".nine")
-        button9.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="9"
-        }) 
-         let button0 = document.querySelector(".zero")
-        button0.addEventListener("click", e=>{
-            e.stopPropagation()
-         document.querySelector(".screen").innerHTML="0"
-        })
+         document.querySelector(".screen").innerHTML="${number}"
+        })`
+        return item;
+}
+main("btn", "one", 0)
